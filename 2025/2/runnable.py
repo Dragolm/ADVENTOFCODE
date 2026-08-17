9d97a9b899acce8e964eea6ff4b571bc51aab33b
dragolm <toolazytothinkofone2@gmail.com>
2026-08-17 18:32:54 +0530

    before arch

--- 2025/2/runnable.py
+++ 2025/2/runnable.py
@@ -1,6 +1,46 @@
+# import re
+# import inspect
+
+# def doit(input):
+#     input_array = input[0].split(",")
+#     ranges = []
+#     for i in input_array:
+#         ranges.append(tuple(i.split("-")))
+
+#     hits = 0
+#     for i in ranges:
+#         start, end = i
+#         for n in range(int(start), int(end)+1):
+#             num = str(n)
+#             l = len(num)
+#             if l%2==0:
+#                 temp = num[:l//2]
+#                 temp = re.search(temp*2, num)
+#                 if temp:
+#                     hits+=int(temp.group(0))
+    
+#     return hits
+
+import re
+import inspect
+
 def doit(input):
     input_array = input[0].split(",")
     ranges = []
     for i in input_array:
         ranges.append(tuple(i.split("-")))
-    print(ranges)
+
+    hits = 0
+    for i in ranges:
+        start, end = i
+        for n in range(int(start), int(end)+1):
+            num = str(n)
+            l = len(num)
+            for i in range(1,l):
+                temp = num[:i]
+                if num==temp*(l//i):
+                    # print(num)
+                    hits+=int(num)
+                    break
+    
+    return hits
